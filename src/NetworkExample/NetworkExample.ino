14e020e999375ac7303ee25dfde3a47d0d9d1684
Tino Hager <tino.hager@nager.at>
2019-08-19 23:58:28 +0200

    add network webserver example

---
+++ src/NetworkExample/NetworkExample.ino
@@ -0,0 +1,114 @@
+#include <ETH.h>
+
+// Variable to store the HTTP request
+String header;
+static bool eth_connected = false;
+WiFiServer server(80);
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.onEvent(WiFiEvent);
+  ETH.begin();
+  server.begin();
+}
+
+void loop()
+{
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (!client) {
+    return;
+  }
+  
+  Serial.println("New Client.");          // print a message out in the serial port
+  String currentLine = "";                // make a String to hold incoming data from the client
+  while (client.connected()) {            // loop while the client's connected
+    if (client.available()) {             // if there's bytes to read from the client,
+      char c = client.read();             // read a byte, then
+      Serial.write(c);                    // print it out the serial monitor
+      header += c;
+      if (c == '\n') {                    // if the byte is a newline character
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+          // and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");
+          client.println();
+
+
+          // Display the HTML web page
+          client.println("<!DOCTYPE html><html>");
+          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          client.println("<link rel=\"icon\" href=\"data:,\">");
+          // CSS to style the on/off buttons
+          // Feel free to change the background-color and font-size attributes to fit your preferences
+          client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+          client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+          client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+          client.println(".button2 {background-color: #555555;}</style></head>");
+
+          // Web Page Heading
+          client.println("<body><h1>ESP32 Web Server</h1>");
+
+          client.println("</body></html>");
+
+          // The HTTP response ends with another blank line
+          client.println();
+          // Break out of the while loop
+          break;
+        } else { // if you got a newline, then clear currentLine
+          currentLine = "";
+        }
+      } else if (c != '\r') {  // if you got anything else but a carriage return character,
+        currentLine += c;      // add it to the end of the currentLine
+      }
+    }
+  }
+  // Clear the header variable
+  header = "";
+  // Close the connection
+  client.stop();
+  Serial.println("Client disconnected.");
+  Serial.println("");
+}
